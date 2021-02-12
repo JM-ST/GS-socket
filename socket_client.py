428013a2a364dd98214a09a245a309a182e7a2b0
supanutkub2 <supanut.tang@ku.th>
2021-02-12 09:25:48 +0700

    add protocol SAL

--- socket_client.py
+++ socket_client.py
@@ -1,17 +1,59 @@
-import socket 
-import webbrowser as ws
+import json
+import socket
+import webbrowser
 
 HOST = "localhost"
 PORT = 9999
 
-socket_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-socket_client.connect((HOST,PORT))
+if __name__ == "__main__":
 
-while True :
+    try:
+        socket_client.connect((HOST, PORT))
+    except socket.error:
+        print("Fail connection with server")
 
-	socket_client.sendall(bytes(input(),'utf-8'))
-	data = socket_client.recv(1024)
-	ws.open(str(data,'utf-8'))
+    while True:
+        try:
+            data = socket_client.recv(1024)
+            data = json.loads(data.decode("utf-8"))
+            print(data["message"])
 
-socket_client.shutdown()
+            if data["STATE"] == "WAITING":
+                keyword = input("Keywords you want to find: ")
+                if keyword != "":
+                    sendto_server = json.dumps(
+                        {
+                            "PROTOCOL": "SAL",
+                            "STATE": "WAITING",
+                            "keyword": keyword
+                        }
+                    )
+                    socket_client.send(bytes(sendto_server, encoding="utf-8"))
+                else:
+                    sendto_server = json.dumps(
+                        {
+                            "PROTOCOL": "SAL",
+                            "STATE": "CLOSE"
+                        }
+                    )
+                    socket_client.send(bytes(sendto_server, encoding="utf-8"))
+
+            elif data["STATE"] == "ACTIVE":
+                sendto_server = json.dumps(
+                    {
+                        "PROTOCOL": "SAL",
+                        "STATE": "ACTIVE",
+                    }
+                )
+                webbrowser.open(data["link"])
+                socket_client.send(bytes(sendto_server, encoding="utf-8"))
+
+            elif data["STATE"] == "CLOSE":
+                socket_client.close()
+                break
+        except socket.error:
+            socket_client.close()
+            print("Something went wrong from server")
+            break
--- socket_server.py
+++ socket_server.py
@@ -1,39 +1,73 @@
 import socket
-
+import json
 
 HOST = "localhost"
 PORT = 9999
 
-socket_server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_server.bind((HOST, PORT))
+socket_server.listen()
 
+if __name__ == "__main__":
+    while True:
+        print("Waiting connection")
+        connection, client_address = socket_server.accept()
 
-socket_server.bind((HOST,PORT))
+        try:
+            data = json.dumps(
+                {
+                    "PROTOCOL": "SAL",
+                    "STATE": "WAITING",
+                    "message": "From server >>> Connection with Server"
+                }
+            )
+            data = bytes(data, encoding="utf-8")
+            connection.send(data)
+            print("Connection:", client_address)
+        except socket.error:
+            print("Fail Connection")
+            continue
 
+        while True:
+            try:
+                data = connection.recv(1024)
+                data = json.loads(data.decode("utf-8"))
 
-socket_server.listen() 
+                if data["STATE"] == "WAITING":
+                    print("Respond to", client_address)
+                    data = json.dumps(
+                        {
+                            "PROTOCOL": "SAL",
+                            "STATE": "ACTIVE",
+                            "message": "From server >>> Success",
+                            "link": "google.com/search?q=%s" % data["keyword"]
+                        }
+                    )
+                    connection.send(bytes(data, encoding="utf-8"))
 
+                elif data["STATE"] == "ACTIVE":
+                    data = json.dumps(
+                        {
+                            "PROTOCOL": "SAL",
+                            "STATE": "WAITING",
+                            "message": "From server >>> Waiting next request from client"
+                        }
+                    )
+                    connection.send(bytes(data, encoding="utf-8"))
 
-while True :
-
-	print("waiting for connection")
-
-	connection, client_adress =  socket_server.accept() 
-
-	try:
-		print(client_adress)
-
-		while True :
-
-			data = connection.recv(1024)
-
-			if data :
-				print("Respond to",client_adress)
-				connection.sendall(bytes("google.com/search?q=",'utf-8')+data)
-		
-		connection.close()
-
-	finally:
-		connection.close()
-		print("close")
-
-
+                elif data["STATE"] == "CLOSE":
+                    data = json.dumps(
+                        {
+                            "PROTOCOL": "SAL",
+                            "STATE": "CLOSE",
+                            "message": "From server >>> Disconnect"
+                        }
+                    )
+                    connection.send(bytes(data, encoding="utf-8"))
+                    connection.close()
+                    print("Disconnect with", client_address)
+                    break
+            except socket.error:
+                connection.close()
+                print("Something went wrong from client")
+                break
